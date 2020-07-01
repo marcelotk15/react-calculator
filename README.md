5b70ead9354ae55b86102e7774ffd856312f1f40
Marcelo Augusto <marcelotk_15@hotmail.com>
2020-07-01 13:36:42 -0300

    app created

--- README.md
+++ README.md
@@ -1,68 +1,59 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Calculator
 
-## Available Scripts
+![image info](./screenshot.png)
 
-In the project directory, you can run:
+**Tier:** 1-Beginner
 
-### `yarn start`
+Calculators are not only one of the most useful tools available, but they are
+also a great way to understand UI and event processing in an application. In
+this problem you will create a calculator that supports basic arithmetic
+calculations on integers.
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+The styling is up to you so use your imagination and get creative! You might
+also find it worth your time to experiment with the calculator app on your
+mobile device to better understand basic functionality and edge cases.
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+### Constraints
 
-### `yarn test`
+- You may not use the `eval()` function to execute calculations
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## User Stories
 
-### `yarn build`
+- [x] User can see a display showing the current number entered or the
+      result of the last operation.
+- [x] User can see an entry pad containing buttons for the digits 0-9,
+      operations - '+', '-', '/', and '=', a 'C' button (for clear), and an 'AC'
+      button (for clear all).
+- [x] User can enter numbers as sequences up to 8 digits long by clicking on
+      digits in the entry pad. Entry of any digits more than 8 will be ignored.
+- [x] User can click on an operation button to display the result of that
+      operation on:
+      _ the result of the preceding operation and the last number entered OR
+      _ the last two numbers entered OR \* the last number entered
+- [x] User can click the 'C' button to clear the last number or the last
+      operation. If the users last entry was an operation the display will be
+      updated to the value that preceded it.
+- [x] User can click the 'AC' button to clear all internal work areas and
+      to set the display to 0.
+- [x] User can see 'ERR' displayed if any operation would exceed the
+      8 digit maximum.
 
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## Bonus features
 
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
+- [ ] User can click a '+/-' button to change the sign of the number that is
+      currently displayed.
+- [x] User can see a decimal point ('.') button on the entry pad to that
+      allows floating point numbers up to 3 places to be entered and operations to
+      be carried out to the maximum number of decimal places entered for any one
+      number.
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+## Useful links and resources
 
-### `yarn eject`
+- [Calculator (Wikipedia)](https://en.wikipedia.org/wiki/Calculator)
+- [MDN](https://developer.mozilla.org/en-US/)
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+## Example projects
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `yarn build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+- [Javascript Calculator](https://codepen.io/giana/pen/GJMBEv)
+- [React Calculator](https://codepen.io/mjijackson/pen/xOzyGX)
+- [Javascript-CALC](https://github.com/x0uter/javascript-calc)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,222 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const MAX_VISOR_CHAR = 8;
+
+  const [total, setTotal] = useState("");
+  const [accumulator, setAccumulator] = useState("");
+  const [saveAction, setSaveAction] = useState(null);
+  const [count, setCount] = useState([]);
+
+  const handleNumber = (e) => {
+    e.preventDefault();
+
+    if (total.length < MAX_VISOR_CHAR) setTotal(total + e.target.textContent);
+  };
+
+  const handleAction = (e) => {
+    e.preventDefault();
+
+    if (total.length === 0) return;
+
+    setAccumulator(`${accumulator} ${total} ${e.target.textContent}`);
+    setTotal("");
+
+    setCount([...count, Number(total), e.target.textContent]);
+  };
+
+  const handleComma = (e) => {
+    e.preventDefault();
+
+    if (!total.includes(".")) setTotal(total + ".");
+  };
+
+  const handleResult = async (e) => {
+    e.preventDefault();
+
+    if (accumulator[accumulator.length - 1] === "=" && total.length > 0)
+      setTotal(
+        ProcessAction(Number(total), Number(total), saveAction)
+          .toString()
+          .substring(0, MAX_VISOR_CHAR)
+      );
+
+    if (count.length === 0) return;
+
+    setCount([...count, Number(total)]);
+
+    setAccumulator(`${accumulator} ${total} =`);
+
+    // await ProcessResult();
+  };
+
+  const ProcessResult = () => {
+    let current = null;
+    let action = null;
+
+    let total = 0;
+
+    if (isNaN(count[count.length - 1])) count.pop();
+
+    count.forEach((n) => {
+      if (!isNaN(n)) {
+        if (current === null) {
+          current = n;
+        } else {
+          total += ProcessAction(current, n, action);
+          current = null;
+        }
+      } else {
+        action = n;
+        setSaveAction(n);
+      }
+    });
+
+    if (current !== null) total = ProcessAction(total, current, action);
+
+    if (total.toString().length > MAX_VISOR_CHAR) return setTotal("ERR");
+
+    setTotal(total.toString().substring(0, MAX_VISOR_CHAR));
+
+    setCount([]);
+  };
+
+  const ProcessAction = (n1, n2, action) => {
+    switch (action) {
+      case "+":
+        return n1 + n2;
+      case "-":
+        return n1 - n2;
+      case "x":
+        return n1 * n2;
+      case "/":
+        return n1 / n2;
+      default:
+        return;
+    }
+  };
+
+  const handleCleanCurrentEntry = (e) => {
+    e.preventDefault();
+
+    setTotal("");
+  };
+
+  const handleCleanAll = (e) => {
+    e.preventDefault();
+
+    setTotal("");
+    setAccumulator("");
+
+    setCount([]);
+  };
+
+  const handlePercentage = (e) => {
+    e.preventDefault();
+
+    if (total !== "") setTotal(Number(total) / 100);
+  };
+
+  const handleSign = (e) => {
+    e.preventDefault();
+
+    setTotal((Number(total) * -1).toString());
+  };
+
+  useEffect(() => {
+    if (!isNaN(count[count.length - 1])) ProcessResult();
+  }, [ProcessResult, count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="calculator">
+        <div className="visor">
+          <div className="accumulator">{accumulator}</div>
+
+          <div className="total">{total}</div>
+        </div>
+
+        <div className="numeric action" onClick={handleCleanAll}>
+          C
+        </div>
+
+        <div className="numeric action" onClick={handleCleanCurrentEntry}>
+          CE
+        </div>
+
+        <div className="numeric action" onClick={handlePercentage}>
+          %
+        </div>
+
+        <div className="numeric action" onClick={handleAction}>
+          /
+        </div>
+
+        <div className="numeric" onClick={handleNumber}>
+          7
+        </div>
+
+        <div className="numeric" onClick={handleNumber}>
+          8
+        </div>
+
+        <div className="numeric" onClick={handleNumber}>
+          9
+        </div>
+
+        <div className="numeric action" onClick={handleAction}>
+          x
+        </div>
+
+        <div className="numeric" onClick={handleNumber}>
+          4
+        </div>
+
+        <div className="numeric" onClick={handleNumber}>
+          5
+        </div>
+
+        <div className="numeric" onClick={handleNumber}>
+          6
+        </div>
+
+        <div className="numeric action" onClick={handleAction}>
+          -
+        </div>
+
+        <div className="numeric" onClick={handleNumber}>
+          1
+        </div>
+
+        <div className="numeric" onClick={handleNumber}>
+          2
+        </div>
+
+        <div className="numeric" onClick={handleNumber}>
+          3
+        </div>
+        <div className="numeric action" onClick={handleAction}>
+          +
+        </div>
+
+        <div className="numeric" onClick={handleSign}>
+          -/+
+        </div>
+
+        <div className="numeric" onClick={handleNumber}>
+          0
+        </div>
+
+        <div className="numeric" onClick={handleComma}>
+          .
+        </div>
+
+        <div className="numeric action result" onClick={handleResult}>
+          =
+        </div>
+      </div>
+    </>
   );
 }
 
